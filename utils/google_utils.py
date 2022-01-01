de3d5b83c66860db4478212a32145d1d6a3fd5d9
viktorstrigeus <viktor.strigeus@gmail.com>
2022-01-01 15:39:19 +0100

    Update google_utils.py

--- utils/google_utils.py
+++ utils/google_utils.py
@@ -36,6 +36,7 @@ def safe_download(file, url, url2=None, min_bytes=1E0, error_msg=''):
 
 def attempt_download(file, repo='ultralytics/yolov5'):
     # Attempt file download if does not exist
+    print("Attempt download "+file.name)
     file = Path(str(file).strip().replace("'", ''))
 
     if not file.exists():
@@ -49,7 +50,8 @@ def attempt_download(file, repo='ultralytics/yolov5'):
         # GitHub assets
         file.parent.mkdir(parents=True, exist_ok=True)  # make parent dir (if required)
         try:
-            response = requests.get(f'https://api.github.com/repos/{repo}/releases/latest').json()  # github api
+            #response = requests.get(f'https://api.github.com/repos/{repo}/releases/latest').json()  # github api
+            response = requests.get(f'https://api.github.com/repos/{repo}/releases/tag/v5.0').json()  # github api
             assets = [x['name'] for x in response['assets']]  # release assets, i.e. ['yolov5s.pt', 'yolov5m.pt', ...]
             tag = response['tag_name']  # i.e. 'v1.0'
         except:  # fallback plan
